46707ea244da02ef2da955bda180ec96351b0f11
henninghk <64491564+henninghk@users.noreply.github.com>
2021-04-22 14:21:38 +0200

    Initial commit

---
+++ burgers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace burgers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
